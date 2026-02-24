5f14b38f524e84569f869b389d6139ddab89418c
FARAJ zakaria <infozakar@gmail.com>
2026-02-24 01:43:23 +0000

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,19 @@
-export { auth as middleware } from '@/auth'
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+import { getToken } from "next-auth/jwt"
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req })
+
+  if (!token && !req.nextUrl.pathname.startsWith("/login")) {
+    return NextResponse.redirect(new URL("/login", req.url))
+  }
+
+  return NextResponse.next()
+}
 
 export const config = {
   matcher: [
-    '/((?!login|api/auth|_next/static|_next/image|favicon.ico).*)',
+    "/((?!login|api/auth|_next/static|_next/image|favicon.ico).*)",
   ],
 }
